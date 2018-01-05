ba0533282b15e9fd94122e3da3d8931ed8642425
Andy Duss <mindovermiles262@gmail.com>
2018-01-05 08:43:19 -0800

    add new book functionality

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 const newBookButton = document.getElementById('new-book')
+const booksTable = document.getElementById('table-books')
 
 let myLibrary = []
 
@@ -15,4 +16,13 @@ function addBookToLibrary(book) {
 
 newBookButton.addEventListener("click", function() {
 	console.log("clicked")
+	var newRow = booksTable.insertRow(1);
+	var newTitle = newRow.insertCell(0);
+	var newAuthor = newRow.insertCell(1);
+	var newPages = newRow.insertCell(2);
+	var newReadStatus = newRow.insertCell(3);
+	newTitle.innerHTML = "1Q84";
+	newAuthor.innerHTML = "Murikami";
+	newPages.innerHTML = 892;
+	newReadStatus.innerHTML = "Yes"
 })
